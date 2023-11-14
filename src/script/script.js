ccf4b5cffe9ae63c3964a3d3031b9ef0b23a87dd
Rahil <rahilsardar234@gmail.com>
2023-11-14 17:10:03 +0530

    added some fuctionalities to text box

---
+++ src/script/script.js
@@ -0,0 +1,20 @@
+var ph
+function changeIcon(icon) {
+    icon.classList.toggle('bi-eye')
+    if (document.getElementById("passbox").type=="text"){
+        document.getElementById("passbox").type="password"
+    }
+    else{
+        document.getElementById("passbox").type="text"
+    }
+}
+function textFocus(element){
+    ph = element.placeholder
+    element.placeholder= ""
+}
+let textOutFocus = function(element){
+    if(element.value==""){
+        element.placeholder= ph
+    }
+
+}
