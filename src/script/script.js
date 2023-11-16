8b9e610306d073a2217f88d5ce0590e4c0b32fa2
Rahil <rahilsardar234@gmail.com>
2023-11-17 02:29:42 +0530

    fixed see password

--- src/script/script.js
+++ src/script/script.js
@@ -1,17 +1,17 @@
 var ph
 var pt
-// function changeIcon(icon) {
-//     icon.classList.toggle("bi-eye")
-//     pt = document.getElementById("passbox").value
-//     pt = document.getElementById("email").value.indexOf("@");
-//     console.log(pt)
-//     if (pt=="text"){
-//         document.getElementById("passbox").type="password"
-//     }
-//     else{
-//         document.getElementById("passbox").type="text"
-//     }
-// }
+
+function changeIcon(icon) {
+    icon.classList.toggle("bi-eye")
+    if (document.getElementById("pass_box").type=="text"){
+        document.getElementById("pass_box").type="password"
+    }
+    else{
+        document.getElementById("pass_box").type="text"
+    }
+    pt = document.getElementById("pass_box").type
+    console.log(pt)
+}
 
 function textFocus(element){
     ph = element.placeholder
@@ -21,12 +21,4 @@ let textOutFocus = function(element){
     if(element.value==""){
         element.placeholder= ph
     }
-}
-function changeIcon(icon) {
-document.addEventListener('DOMContentLoaded', function () {
-        icon.classList.toggle("bi-eye")
-        pt = document.getElementById("passbox").value
-        // pt = document.getElementById("email").value.indexOf("@");
-        console.log(pt)
-    });
-}  
+} 
