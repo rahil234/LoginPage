d19419413e69d849d5664f0706ae81aed905f138
Rahil <rahilsardar234@gmail.com>
2023-11-17 00:46:55 +0530

    make ui more responsive

--- src/script/script.js
+++ src/script/script.js
@@ -1,13 +1,18 @@
 var ph
-function changeIcon(icon) {
-    icon.classList.toggle("bi-eye")
-    if (document.getElementById("passbox").type=="text"){
-        document.getElementById("passbox").type="password"
-    }
-    else{
-        document.getElementById("passbox").type="text"
-    }
-}
+var pt
+// function changeIcon(icon) {
+//     icon.classList.toggle("bi-eye")
+//     pt = document.getElementById("passbox").value
+//     pt = document.getElementById("email").value.indexOf("@");
+//     console.log(pt)
+//     if (pt=="text"){
+//         document.getElementById("passbox").type="password"
+//     }
+//     else{
+//         document.getElementById("passbox").type="text"
+//     }
+// }
+
 function textFocus(element){
     ph = element.placeholder
     element.placeholder= ""
@@ -17,3 +22,11 @@ let textOutFocus = function(element){
         element.placeholder= ph
     }
 }
+function changeIcon(icon) {
+document.addEventListener('DOMContentLoaded', function () {
+        icon.classList.toggle("bi-eye")
+        pt = document.getElementById("passbox").value
+        // pt = document.getElementById("email").value.indexOf("@");
+        console.log(pt)
+    });
+}  
