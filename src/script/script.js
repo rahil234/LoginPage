3a47a5ea414009b9a788e0209b8e8f5e7984bebe
Rahil <rahilsardar234@gmail.com>
2023-11-17 22:11:30 +0530

    fixed issues with placeholder

--- src/script/script.js
+++ src/script/script.js
@@ -1,21 +1,25 @@
-var ph
+let email_ph = document.getElementById("email_box").placeholder;
+let pass_ph = document.getElementById("pass_box").placeholder;
 
 function changeIcon(icon) {
-    icon.classList.toggle("bi-eye")
-    if (document.getElementById("pass_box").type=="text"){
-        document.getElementById("pass_box").type="password"
-    }
-    else{
-        document.getElementById("pass_box").type="text"
-    }
+  icon.classList.toggle("bi-eye");
+  if (document.getElementById("pass_box").type == "text") {
+    document.getElementById("pass_box").type = "password";
+  } else {
+    document.getElementById("pass_box").type = "text";
+  }
 }
 
-function textFocus(element){
-    ph = element.placeholder
-    element.placeholder= ""
+function textFocus(element) {
+  element.placeholder = "";
 }
-let textOutFocus = function(element){
-    if(element.value==""){
-        element.placeholder= ph
+
+function textOutFocus(element) {
+  if (element.value == "") {
+    if (element.type == "text") {
+      element.placeholder = email_ph;
+    } else if (element.type == "password") {
+      element.placeholder = pass_ph;
     }
-} 
+  }
+}
