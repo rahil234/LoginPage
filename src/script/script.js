b7b456289e50268357339b258ea1a38b7b97976d
Rahil <rahilsardar234@gmail.com>
2023-11-19 11:54:18 +0530

    formatted script.js

--- src/script/script.js
+++ src/script/script.js
@@ -5,39 +5,31 @@ let holdstart = () => {
   document.querySelector("#see_pass").setAttribute("src", "src/img/eye-fill.svg");
   document.querySelector("#pass_box").setAttribute("type", "text");
 };
-
 let holdend = () => {
   document.querySelector("#see_pass").setAttribute("src", "src/img/eye-slash.svg");
   document.querySelector("#pass_box").setAttribute("type", "password");
   document.querySelector("#pass_box").focus();
 };
+let see_pass_check = () => {
+  if (document.querySelector("#pass_box").value !== "") document.querySelector("#see_pass").style.visibility = "visible"
+  else if (document.querySelector("#pass_box").value === "") document.querySelector("#see_pass").style.visibility = "hidden"
+}
 
 document.querySelector("#see_pass").addEventListener("touchstart", holdstart);
 document.querySelector("#see_pass").addEventListener("mousedown", holdstart);
 document.querySelector("#see_pass").addEventListener("touchend", holdend);
 document.querySelector("#see_pass").addEventListener("mouseup", holdend);
 
-let see_pass_check = () => {
-  if (document.querySelector("#pass_box").value !== "") document.querySelector("#see_pass").style.visibility = "visible"
-  else if (document.querySelector("#pass_box").value === "") document.querySelector("#see_pass").style.visibility = "hidden"
-}
+document.querySelector("#pass_box").addEventListener("keydown", () =>
+  setTimeout(see_pass_check, 30)
+);
 
-document.querySelector("#pass_box").addEventListener("keydown", () => {
-  setTimeout(see_pass_check, 30);
-});
-
-document
-  .querySelector("#email_box")
-  .addEventListener(
-    "focus",
-    () => (document.querySelector("#email_box").placeholder = "")
-  );
-document
-  .querySelector("#email_box")
-  .addEventListener(
-    "focusout",
-    () => (document.querySelector("#email_box").placeholder = email_ph)
-  );
+document.querySelector("#email_box").addEventListener("focus", () =>
+  document.querySelector("#email_box").placeholder = ""
+);
+document.querySelector("#email_box").addEventListener("focusout", () =>
+  document.querySelector("#email_box").placeholder = email_ph
+);
 document.querySelector("#pass_box").addEventListener("focus", () => {
   document.querySelector("#pass_box").placeholder = "";
 });
