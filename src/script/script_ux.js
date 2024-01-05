4a17d5931e471267dad8287d31080a65f2508553
Rahil <rahilsardar234@gmail.com>
2024-01-05 23:46:32 +0530

    final commit

--- src/script/script_ux.js
+++ src/script/script_ux.js
@@ -1,29 +1,6 @@
 const email_ph = document.querySelector("#email_box").placeholder;
 const pass_ph = document.querySelector("#pass_box").placeholder;
 
-let holdstart = () => {
-  document.querySelector("#see_pass").setAttribute("src", "src/img/eye-fill.svg");
-  document.querySelector("#pass_box").setAttribute("type", "text");
-};
-let holdend = () => {
-  document.querySelector("#see_pass").setAttribute("src", "src/img/eye-slash.svg");
-  document.querySelector("#pass_box").setAttribute("type", "password");
-  document.querySelector("#pass_box").focus();
-};
-let see_pass_check = () => {
-  if (document.querySelector("#pass_box").value !== "") document.querySelector("#see_pass").style.visibility = "visible"
-  else if (document.querySelector("#pass_box").value === "") document.querySelector("#see_pass").style.visibility = "hidden"
-}
-
-document.querySelector("#see_pass").addEventListener("touchstart", holdstart);
-document.querySelector("#see_pass").addEventListener("mousedown", holdstart);
-document.querySelector("#see_pass").addEventListener("touchend", holdend);
-document.querySelector("#see_pass").addEventListener("mouseup", holdend);
-
-document.querySelector("#pass_box").addEventListener("keydown", () =>
-  setTimeout(see_pass_check, 30)
-);
-
 document.querySelector("#email_box").addEventListener("focus", () =>
   document.querySelector("#email_box").placeholder = ""
 );
@@ -46,3 +23,26 @@ document.querySelector("#check").addEventListener("click", () => {
     document.querySelector("#check").setAttribute("src", "src/img/check-square-fill.svg")
   else  document.querySelector("#check").setAttribute("src", "src/img/check-square.svg")
 });
+
+let holdstart = () => {
+  document.querySelector("#see_pass").setAttribute("src", "src/img/eye-fill.svg");
+  document.querySelector("#pass_box").setAttribute("type", "text");
+};
+let holdend = () => {
+  document.querySelector("#see_pass").setAttribute("src", "src/img/eye-slash.svg");
+  document.querySelector("#pass_box").setAttribute("type", "password");
+  document.querySelector("#pass_box").focus();
+};
+let see_pass_check = () => {
+  if (document.querySelector("#pass_box").value !== "") document.querySelector("#see_pass").style.visibility = "visible"
+  else if (document.querySelector("#pass_box").value === "") document.querySelector("#see_pass").style.visibility = "hidden"
+}
+
+document.querySelector("#see_pass").addEventListener("touchstart", holdstart);
+document.querySelector("#see_pass").addEventListener("mousedown", holdstart);
+document.querySelector("#see_pass").addEventListener("touchend", holdend);
+document.querySelector("#see_pass").addEventListener("mouseup", holdend);
+
+document.querySelector("#pass_box").addEventListener("keydown", () =>
+  setTimeout(see_pass_check, 30)
+);
